a30b0f8559e6285180acd18d82e69a0c937048ee
xindi <dezhangxd@163.com>
2026-05-31 17:46:00 +0800

    upd

--- js/data/publications.js
+++ js/data/publications.js
@@ -260,7 +260,7 @@ const JOURNAL_PAPERS = [
     venue: 'JOS', venueFull: '软件学报',
     year: 2026, ccf: 'A',
     links: [
-      { label: 'Paper', url: 'https://jos.org.cn/jos/article/abstract/7509', type: 'paper' }
+      { label: 'Paper', url: 'https://jos.org.cn/jos/article/abstract/7509', type: 'paper' },
       { label: 'Code', url: 'https://github.com/shaowei-cai-group/cold-restart', type: 'code' }
     ]
   },
